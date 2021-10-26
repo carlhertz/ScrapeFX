5de3535095e198e2309020312e10c7551d677e06
carlhertz <carlhertz@yahoo.com>
2021-10-26 14:21:19 -0500

    Add files via upload
    - browser html display
    - starting to break up excess functionality

---
+++ src/web/webDisplay.java
@@ -0,0 +1,36 @@
+package web;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+
+import javafx.scene.control.TextArea;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+public class webDisplay {
+	
+	 void displayWeb(TextArea docTextArea, WebView webPanel) throws IOException {
+	    	String htmlbody = docTextArea.getText();
+	    	File newHtmlFile = new File("src/pageHTML.html");
+	        BufferedWriter writer;
+			try {
+				writer = new BufferedWriter(new FileWriter(newHtmlFile));
+		        writer.write(htmlbody);
+		        
+		        writer.close();
+		        
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	        WebEngine engine = webPanel.getEngine();
+
+	        URL urlHello = new File("src/pageHTML.html").toURI().toURL();
+	        engine.load(urlHello.toExternalForm());
+	        
+	    }
+}
